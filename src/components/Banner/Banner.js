13ab18a7e740333e403958dfc7ae9bfd79005cf0
mahmudunnaby <mahmudunnabyemon0@gmail.com>
2022-07-25 04:56:34 +0600

    nav bar

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import banner from '../../assets/Rectangle 2.png'
+import styles from './Banner.css'
+
+const Banner = () => {
+    return (
+        <div className='image-overlay'>
+            <div className=''></div>
+            <h1>Computer Engineering</h1>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/components/Shared/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { InputGroup, Form } from 'react-bootstrap';
+import { AiOutlineSearch, AiFillCaretDown } from 'react-icons/ai';
+import image from '../../assets/whole.png'
+import styles from './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav className=''>
+            <div className='container'>
+                <div className='d-flex justify-content-between align-items-center   my-2'>
+                    <img src={image} alt="" />
+
+
+
+                    <div className=" ps-2 input-group search-box  d-flex justify-content-center align-items-center">
+                        <AiOutlineSearch />
+
+                        <input type="text" className="form-control  mx-3" id='search-box-2' placeholder='Search for your favorite groups in ATG' />
+
+
+                    </div>
+
+
+                    <div className='create-account'>
+                        <p className='create-account-text mt-3'>Create account. <span className='create-account-text-2'>It’s free!</span></p>
+                        <AiFillCaretDown />
+                    </div>
+
+                </div>
+
+            </div>
+
+        </nav >
+    );
+};
+
+export default Navbar;
